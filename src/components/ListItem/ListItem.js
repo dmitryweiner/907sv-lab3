2b213d8c598e33e8e9c3dde6fc449df07e95844b
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-03-09 14:39:25 +0700

    Refactor ListItem Component to add checkbox functionality

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './ListItem.css';
 
-export default function ListItem({ title, id, handleRemove }) {
+export default function ListItem({ title, id, handleRemove, isChecked, handleChecked }) {
   return (
     <>
       <div>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          value={isChecked}
+          data-testid="checkbox"
+          onClick={() => handleChecked(id)}
+        />
         <div className="task" data-testid="task">
           {title}
         </div>
