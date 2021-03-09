99741042edd50425f394ac70a34a15aeca5b61f2
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-03-09 14:29:43 +0700

    Added tests for ListItem checkbox

--- src/components/ListItem/ListItem.test.js
+++ src/components/ListItem/ListItem.test.js
@@ -5,20 +5,84 @@ import ListItem from './ListItem';
 const task = 'Принять таблетки';
 const id = 1;
 const handleRemove = jest.fn();
+const handleChecked = jest.fn();
+const checked = false;
 
-test('ListItem to have given text', () => {
-  render(<ListItem title={task} id={id} handleClick={handleRemove} />);
+test('Отображает на экране то, что передали в пропсе title', () => {
+  render(
+    <ListItem
+      title={task}
+      id={id}
+      handleRemove={handleRemove}
+      isChecked={checked}
+      handleChecked={handleChecked}
+    />
+  );
   const element = screen.getByTestId('task');
   expect(element).toBeInTheDocument();
   expect(element).toHaveTextContent(task);
   screen.getByText(content => content.startsWith('таблетки', 8));
 });
 
-test('ClickHandler to be called with id', () => {
-  render(<ListItem title={task} id={id} handleRemove={handleRemove} />);
+test('При нажатии на кнопку должен вызываться переданный в пропсах коллбэк handleRemove с параметром id', () => {
+  render(
+    <ListItem
+      title={task}
+      id={id}
+      handleRemove={handleRemove}
+      isChecked={checked}
+      handleChecked={handleChecked}
+    />
+  );
   const button = screen.getByTestId('test-button');
   expect(button).toBeInTheDocument();
   expect(handleRemove).not.toBeCalled();
   fireEvent.click(button);
   expect(handleRemove).toBeCalledWith(id);
+});
+
+test('Должен показывать на экране чекбокс', () => {
+  render(
+    <ListItem
+      title={task}
+      id={id}
+      handleRemove={handleRemove}
+      isChecked={checked}
+      handleChecked={handleChecked}
+    />
+  );
+  const checkbox = screen.getByTestId('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  expect(checkbox).toBeVisible();
+});
+
+test('Состояние чекбокса зависит от переданного пропса checked.', () => {
+  render(
+    <ListItem
+      title={task}
+      id={id}
+      handleRemove={handleRemove}
+      isChecked={checked}
+      handleChecked={handleChecked}
+    />
+  );
+  const checkbox = screen.getByTestId('checkbox');
+  expect(checkbox.checked).toEqual(checked);
+});
+
+test('При клике на чекбокс должен вызываться коллбэк handleChecked с параметром id', () => {
+  render(
+    <ListItem
+      title={task}
+      id={id}
+      handleRemove={handleRemove}
+      isChecked={checked}
+      handleChecked={handleChecked}
+    />
+  );
+  const checkbox = screen.getByTestId('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  expect(handleChecked).not.toBeCalled();
+  fireEvent.click(checkbox);
+  expect(handleChecked).toBeCalledWith(id);
 });
