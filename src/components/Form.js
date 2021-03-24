7855e94e7edb9aa7a59990a36c1437c7f988958d
Artemius <artemcka.jdanov@yandex.ru>
2021-03-24 16:26:31 +0700

    fixing a bug with an empty element

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 export default function Form({ handleSubmit }) {
   const [value, setValue] = React.useState('');
+  const [alert, setAlert] = React.useState('');
 
   function submitHandler(e) {
     e.preventDefault();
     if (value.length === 0) {
-      alert('Пусто');
+      setAlert('Пустая задача');
+    } else {
+      handleSubmit(value);
+      setAlert('');
     }
-    handleSubmit(value);
     setValue('');
   }
 
   return (
     <form data-testid="form" onSubmit={submitHandler}>
+      <div className="alertSend">{alert}</div>
       <input data-testid="input" value={value} onChange={e => setValue(e.target.value)} />
       <button type="submit">Save</button>
     </form>
--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -18,3 +18,20 @@ test('Проверка формы', () => {
   expect(handleSubmit).toBeCalledWith(text);
   expect(screen.getByTestId('input')).toHaveValue('');
 });
+
+test('Проверка валидации', () => {
+  const value = '';
+  const handleSubmit = jest.fn();
+  render(<Form handleSubmit={handleSubmit} />);
+  const input = screen.getByTestId('input');
+  fireEvent.input(input, {
+    target: {
+      value: value
+    }
+  });
+  expect(handleSubmit).not.toBeCalled();
+  const form = screen.getByTestId('form');
+  fireEvent.submit(form);
+  expect(handleSubmit).not.toBeCalled();
+  expect(screen.getByText('Пустая задача')).toBeInTheDocument();
+});
