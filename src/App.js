380cc85c6ecfa9e6dc90ec7947cfcd5a1fa1d6fb
explrms <imExpelliarmus@yandex.ru>
2021-04-19 11:16:48 +0700

    • Валидация формы и фикс тестов

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
-import List from './components/Form';
+import List from './components/List';
 
 export default function App() {
   const [list, setList] = useState([]);
@@ -14,12 +14,14 @@ export default function App() {
       isChecked: false
     };
 
-    if (value === '') {
-      alert('Пожалуйста, введите текст');
-    } else {
-      setList([...list, newValue]);
-    }
+    setList([...list, newValue]);
   }
+
+  // function reducer(action) {
+  //     switch (action.type) {
+  //       case add():
+  //     }
+  // }
 
   function remove(id) {
     setList([...list.filter(item => item.id !== id)]);
--- src/components/tests/Form.test.js
+++ src/components/tests/Form.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Form from '../Form';
 import React from 'react';
 
-test('The form allows you to enter data, calls the handler', () => {
+test('Форма позволяет вводить данные, вызывает обработчик', () => {
   const value = '19';
   const handleSubmit = jest.fn();
 
@@ -19,4 +19,24 @@ test('The form allows you to enter data, calls the handler', () => {
   const form = screen.getByTestId('form');
   fireEvent.submit(form);
   expect(handleSubmit).toBeCalledWith(value);
+});
+
+test('Валидация', () => {
+  const value = '';
+  const handleSubmit = jest.fn();
+
+  render(<Form handleSubmit={handleSubmit} />);
+
+  const input = screen.getByTestId('input');
+  fireEvent.input(input, {
+    target: {
+      value: value
+    }
+  });
+  expect(handleSubmit).not.toBeCalled();
+
+  const form = screen.getByTestId('form');
+  fireEvent.submit(form);
+  expect(handleSubmit).not.toBeCalled();
+  expect(screen.getByText('Введите текст, пожалуйста')).toBeInTheDocument();
 });
